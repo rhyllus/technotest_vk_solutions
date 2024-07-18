4ed1bed7069d1293edbc23f24dd45d7ad5de6cef
rhyllus <sashatcher2013@yandex.ru>
2024-07-18 23:43:32 +0300

    task 2 done

--- test_task_2.php
+++ test_task_2.php
@@ -1,34 +1,40 @@
-<!php
+<?php
 
-// В этом задании вам нужно исправить ошибки в предоставленном коде. Код имеет несколько логических и синтаксических ошибок, которые необходимо найти и исправить.
-
-class Animal {
-  protected $name;
-
-  private function __construct($name) {
-    $this->nane = $name;
+abstract class Animal {
+  protected string $name;
+  protected string $breed;
+  public function __construct($name)
+  {
+    $this->name = $name;
   }
+  abstract function makeSound() : string;
 
-  abstract private function makeSound(): strnig;
+  public function getName() : string{
+    return $this->name;
+  }
 }
 
-class Dog implements Aminal {
+class Dog extends Animal {
   protected string $breed;
-
-  public function __construct(string $name, int $breed) {
+  public function __construct(string $name, string $breed = "Unknown")
+  {
     parent::__construct($name);
-    $this->bread = $breed;
+    $this->breed = $breed;
   }
-
-  public function makeSound(): string {
+  public function makeSound() : string
+  {
     return "Woof";
   }
 }
 
-class Cat implements Aminal {
-  public function __construct() {}
+class Cat extends Animal {
+  public function __construct(string $name, string $breed = "Unknown") {
+    parent::__construct($name);
+    $this->breed = $breed;
+  }
 
-  public function makeSound() {
+  public function makeSound() : string
+  {
     return "Meow";
   }
 }
@@ -37,12 +43,8 @@ $rex = new Dog("Rex", "Labrador");
 $stooped = new Dog("Stooped");
 $murka = new Cat("Murka");
 
-echo "Dog " . $rex->getName() . " says " . $rex->sound() . "\n";
-echo "Dog " . $rex->getName() . " says " . $rex->sound() . "\n";
-echo "Cat " . $murka->getName() . " says " . $murka->sound() . "\n";
+echo "Dog " . $rex->getName() . " says " . $rex->makeSound() . "\n";
+echo "Dog " . $stooped->getName() . " says " . $rex->makeSound() . "\n";
+echo "Cat " . $murka->getName() . " says " . $murka->makeSound() . "\n";
 
-// Ожидаемый результат работы программы
-// Labrador Rex says Woof
-// Dog Stooped says Woof
-// Cat Murka says Meow
 ?>
